ec95c513c003ed5c420a4ea2149d5da187d7ff0b
DarkCoder100 <singhsima725@gmail.com>
2021-06-10 19:41:38 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+// import StateWise from './components/statewisedata/StateWise';
+import MainPage from "./components/ourpage/mainPage/MainPage";
+// Pages
+import About from "./components/ourpage/About";
+import Services from "./components/ourpage/Services";
+import Contact from "./components/ourpage/Contact";
+import Tracker from "./components/ourpage/Tracker";
+// Modules
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <MainPage exact path="/" />
+        <Route exact path="/about" component={About}></Route>
+        <Route exact path="/services" component={Services}></Route>
+        <Route exact path="/contact" component={Contact}></Route>
+        <Route exact path="/tracker" component={Tracker}></Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/ourpage/Tracker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Tracker = () => {
+    return (
+        <div>
+            Tracker Page
+        </div>
+    )
+}
+
+export default Tracker
